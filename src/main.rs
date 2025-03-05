31958166c4b2d622c62e8d6d5127b7577471583e
skyeBreach <skyebenson@hotmail.com>
2025-03-05 21:36:01 +0000

    feat: cleanup and impl initial main and run func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    // Run the main process and receive its result
+    let result = run();
+
+    // Attempt to gracefully exit with result and output `ExitCode`
+    match result {
+        Ok(code) => return code,
+        Err(e) => {
+            // For now print error to console and exit gracefully with general error exit code.
+            // NOTE: This should be altered later to actually be useful
+            eprintln!("Error: {e}");
+            return ExitCode::from(2);
+        }
+    }
+}
+
+fn run() -> Result<ExitCode, Box<dyn std::error::Error>> {
+    Ok(ExitCode::SUCCESS)
 }
